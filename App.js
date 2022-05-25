80639471dafe4db367792d11b727f107f7c49987
Jena Roberts <jenaroberts@Jenas-MacBook-Air.local>
2022-05-25 16:14:29 -0400

    react-native-app-mobile

--- App.js
+++ App.js
@@ -1,11 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ActivityIndicator,
+  ImageBackground,
+  ScrollView,
+  Button,
+} from "react-native";
+const image = {
+  uri: "https://previews.123rf.com/images/kesu87/kesu871904/kesu87190400027/120480515-greek-food-background-traditional-different-greek-dishes-top-view-close-up.jpg",
+};
 
 export default function App() {
+  const [allRestaurants, setAllRestaurants] = useState();
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await fetch(
+          "https://my-first-firestore-jr.web.app/restaurants"
+        );
+        const data = await response.json();
+        console.log(data);
+        setAllRestaurants(data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    getData();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground
+        resizeMode="cover"
+        source={image}
+        style={styles.container}
+      >
+        <ScrollView>
+          {allRestaurants ? (
+            allRestaurants.map((singleRest) => (
+              <Text style={styles.restaurantsName} key={singleRest.id}>
+                {singleRest.name}
+              </Text>
+            ))
+          ) : (
+            <ActivityIndicator size="large" color="purple" />
+          )}
+        </ScrollView>
+        <Button title="Learn More" color="#007AFF" />
+        <Text>Hey class what up!</Text>
+        <StatusBar style="auto" />
+      </ImageBackground>
     </View>
   );
 }
@@ -13,8 +61,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    width: "100%",
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  restaurantsName: {
+    color: "",
+    textShadowColor: "black",
+    textShadowRadius: "4",
+    fontSize: 73,
+    marginVertical: 50,
   },
 });
